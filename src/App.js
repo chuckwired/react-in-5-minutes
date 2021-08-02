52fbf16ce803fe1def4c39f4fc51c2bcf00458fa
Chuck Rice <chuck@residently.com>
2021-08-02 16:51:13 +0100

    step 2 add a raw useState for counter value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const value = useState(0);
+  console.log(value); // [0, ƒ()]
+
   return (
     <div>
-      <h1>0</h1>
+      <h1>{value[0]}</h1>
       <button>Change!</button>
     </div>
   );
