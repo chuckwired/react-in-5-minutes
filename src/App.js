03f05449fa115ebcaf765fbc5f51e5221f4c3ce9
Chuck Rice <chuck@residently.com>
2021-08-02 16:59:21 +0100

    step 5 refactor for cleaner onClick functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // remember, there's a second item from the array that's missing here, but we'll come right back to use it soon
   const [count, setCount] = useState(0);
+
+  const change = () => {
+    setCount(prevCount => prevCount + 1);
+  }
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+      <button onClick={change}>
         Change!
       </button>
     </div>
