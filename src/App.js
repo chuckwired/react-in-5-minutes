9a0481bc864b52182d3df2ff1efe8a3ebb7c550d
Chuck Rice <chuck@residently.com>
2021-08-02 16:57:59 +0100

    step 4 add a basic setCount to update counter value

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useState } from 'react';
 
 function App() {
   // remember, there's a second item from the array that's missing here, but we'll come right back to use it soon
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>{count}</h1>
-      <button>Change!</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Change!
+      </button>
     </div>
   );
 }
