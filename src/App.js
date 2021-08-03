6bf84e6af5ba06629de27234a571aaea6e3b3f68
Chuck Rice <chuck@residently.com>
2021-08-03 22:07:46 +0100

    step 7: add randomcolor and update number color BUT with infinite component updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { useState, useEffect } from 'react';
+import randomcolor from 'randomcolor';
 
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('') // initial value can be an empty string
 
   const change = () => {
     setCount(prevCount => prevCount + 1);
   }
 
+  // useEffect will run every time the component re-renders
   useEffect(() => {
-    /* any update can happen here */
+    setColor(randomcolor())
   });
 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1 style={{ color: color }}>{count}</h1>
       <button onClick={change}>
         Change!
       </button>
